bc23976ebd672096add781fbdd0768d060a6cefc
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-02-04 09:55:57 +0300

    Update union.c

--- union.c
+++ union.c
@@ -8,25 +8,25 @@ int main(int argc, char **argv)
         return 0;
     }
     int c[255] = {0};
-    int i = 0;
     char *s1 = argv[1];
     char *s2 = argv[2];
+    int i = 0;
     while (s1[i] != '\0')
     {
-        if (c[s1[i] - '0'] == 0)
+        if (c[(int)s1[i] - '0'] == 0)
         {
             write(1, &s1[i], 1);
-            c[s1[i] - '0'] = 1;
+            c[(int)s1[i] - '0'] = 1;
         }
         i++;
     }
     i = 0;
     while (s2[i] != '\0')
     {
-        if (c[s2[i]- '0'] == 0)
+        if (c[(int)s2[i] - '0'] == 0)
         {
             write(1, &s2[i], 1);
-            c[s2[i]- '0'] = 1;
+            c[(int)s2[i]- '0'] = 1;
         }
         i++;
     }
