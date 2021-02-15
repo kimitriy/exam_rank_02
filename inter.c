975b66c6521700df87c3f3cdce4298051bf993a1
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-02-16 02:01:02 +0300

    Add files via upload

--- inter.c
+++ inter.c
@@ -1,31 +1,34 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
-    int c[255] = {0};
-    char *s1 = argv[1];
-    char *s2 = argv[2];
-    int i = 0;
-    int j = 0;
-    while (s1[i] != '\0')
-    {
-        j = 0;
-        while(s2[j] != '\0')
-        {
-            if (s1[i] == s2[j] && c[(int)s1[i]] == 0)
-            {
-                write(1, &s1[i], 1);
-                c[(int)s1[i]] = 1;
-            }
-            j++;
-        }
-        i++;
-    }
-    write(1, "\n", 1);
-    return 0;
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	int		c[255] = {0};
+	char	*s1 = argv[1];
+	char	*s2 = argv[2];
+	int		i = 0;
+	int		j = 0;
+
+	while (s1[i] != '\0')
+	{
+		while (s2[j] != '\0')
+		{
+			if (s1[i] == s2[j] && c[(int)s1[i]] == 0)
+			{
+				write(1, &s1[i], 1);
+				c[(int)s1[i]] = 1;
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+
+	write(1, "\n", 1);
+	return (0);
 }
--- union.c
+++ union.c
@@ -1,35 +1,38 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        write (1, "\n", 1);
-        return 0;
-    }
-    int c[255] = {0};
-    char *s1 = argv[1];
-    char *s2 = argv[2];
-    int i = 0;
-    while (s1[i] != '\0')
-    {
-        if (c[(int)s1[i] - '0'] == 0)
-        {
-            write(1, &s1[i], 1);
-            c[(int)s1[i] - '0'] = 1;
-        }
-        i++;
-    }
-    i = 0;
-    while (s2[i] != '\0')
-    {
-        if (c[(int)s2[i] - '0'] == 0)
-        {
-            write(1, &s2[i], 1);
-            c[(int)s2[i]- '0'] = 1;
-        }
-        i++;
-    }
-    write(1, "\n", 1);
-    return 0;
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	int		c[255] = {0};
+	char	*s1 = argv[1];
+	char	*s2 = argv[2];
+	int		i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (c[(int)s1[i] - '0'] == 0)
+		{
+			write(1, &s1[i], 1);
+			c[(int)s1[i] - '0'] = 1;
+		}
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		if (c[(int)s2[i] - '0'] == 0)
+		{
+			write(1, &s2[i], 1);
+			c[(int)s2[i] - '0'] = 1;
+		}
+		i++;
+	}
+
+	write(1, "\n", 1);
+	return (0);
 }
