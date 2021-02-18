63df8246b1b44cb865191acc28ba0375cb325200
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-02-18 23:10:29 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -2,48 +2,102 @@
 
 int		get_next_line(char **line)
 {
-	char	buf[100000] = {'\0'};
+	char	buf[1000000] = {'\0'};
 	char	c;
 	int		rv = 0;
 	int		i = -1;
 
-	while ((rv = read(0, &c, 1)) > 0 && c != '\0' && c != '\n')
+	while ((rv = read(0, &c, 1)) > 0 &&  c!= '\0' && c != '\n')
 		buf[++i] = c;
-	*line = buf;
-	return (rv < 0 ? -1 : rv);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-	int		rv;
-	char	*line;
-
-	while ((rv = get_next_line(&line)) > 0)
-		printf("%s\n", line);
-	printf("%s", line);
-}
-
-
-
-int		get_next_line(char **line)
-{
-	char	buf[100000] = {'\0'};
-	char	c;
-	int		rv = 0;
-	int		i = -1;
-
-	while ((rv = read(0, &c, 1)) > 0 && c != '\0' && c != '\n')
-		buf[++i] = c;
-	if (!(*line = (char*)malloc(100000)))
+	if (rv < 0)
 		return (-1);
-	i = -1;
-	while (buf[++i] != '\0')
+	if (!(*line = (char*)malloc(i + 1)))
+		return (-1);
+	i = 0;
+	while (buf[i] != '\0')
+	{
 		line[0][i] = buf[i];
+		i++;
+	}
 	line[0][i] = '\0';
-	return (rv < 0 ? -1 : rv);
+	return (rv);
 }
+
+// int		get_next_line(char **line)
+// {
+// 	char	c;
+// 	char	*buf;
+// 	int		rv;
+// 	int		i = 0;
+
+// 	buf = (char *)malloc(10000);
+// 	while ((rv = read(0, &c, 1)) > 0 && c != '\n' && c != '\0')
+// 	{
+// 		buf[i] = c;
+// 		i++;
+// 	}
+// 	buf[i] = '\0';
+// 	if (rv == -1)
+// 		return (-1);
+// 	*line = buf;
+// 	return (rv ? 1 : 0);
+// }
+
+// int		get_next_line(char **line)
+// {
+// 	int	res = 1;
+// 	int i = 0;
+// 	char *buf;
+// 	char c;
+
+// 	buf = malloc(sizeof(char) * 10000);
+// 	if (buf == NULL)
+// 		return (-1);
+
+// 	while (i < 10000)
+// 		buf[i++] = '\0';
+	
+// 	i = 0;
+// 	while (res > 0)
+// 	{
+// 		res = read(0, &c, 1);
+// 		if (res > 0 && c != '\n')
+// 			buf[i++] = c;
+// 		if (res > 0 && c == '\n')
+// 		{
+// 			*line = buf;
+// 			return (1);
+// 		}	
+// 	}
+// 	*line = buf;
+// 	return (0);
+// }
+
+// int		get_next_line(char **line)
+// {
+//   int i;
+//   int ret;
+//   char buffer[1000000];
+//   char buf;
+//   i = 0;
+//   while ((ret = read(0, &buf, 1)) != 0)
+//   {
+//     if (buf == '\n')
+//       rveak ;
+//     buffer[i] = buf;
+//     i++;
+//   }
+//   buffer[i] = '\0';
+//   *line = (char *)malloc(sizeof(char) * i);
+//   i = 0;
+//   while (buffer[i] != 0)
+//   {
+//     line[0][i] = buffer[i];
+//     i++;
+//   }
+//   line[0][i] = '\0';
+//   return (ret);
+// }
 
 #include <stdio.h>
 
@@ -52,11 +106,15 @@ int main(void)
 	int		rv;
 	char	*line;
 
-	while ((rv = get_next_line(&line)) > 0)
+	while (1)
 	{
-		printf("%s\n", line);
+		while ((rv = get_next_line(&line)) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
+		printf("%s", line);
 		free(line);
 	}
-	printf("%s", line);
-	free(line);
+	return (0);
 }
